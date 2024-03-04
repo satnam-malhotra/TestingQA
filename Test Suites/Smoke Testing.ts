823215436ac5adf9890386dec5453662d109aa76
Satnam Malhotra <satnam.malhotra@office365.3pillarglobal.com>
2024-03-04 22:06:55 +0530

    Added test suite

---
+++ Test Suites/Smoke Testing.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2891f5bb-4b0a-4238-bc42-8f16d4f9a6cd</testSuiteGuid>
+   <testCaseLink>
+      <guid>cec7c4b5-4b5f-4dc2-be43-813d04c55497</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Verify Hompegae</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
